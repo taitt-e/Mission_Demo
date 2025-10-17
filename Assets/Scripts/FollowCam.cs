1b12332467f1fd7cd226e188e14fd07ecc8f9554
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-10-17 01:32:39 -0500

    Finish followcam

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FollowCam : MonoBehaviour
 {
     static public GameObject POI; // The Static point of interest
+    [Header("Inscribed")]
+    public float easing = 0.05f;
 
     [Header("Dynamic")]
     public float camZ; // The desired Z pos of the camera
@@ -16,14 +18,30 @@ public class FollowCam : MonoBehaviour
 
     void FixedUpdate()
     {
-        // A single-line if statement doesn't require braces
-        if (POI == null) return;
-
-        // Get the poisition of the poi
-        Vector3 destination = POI.transform.position;
+        Vector3 destination = Vector3.zero;
+        if (POI != null)
+        {
+            // If the POI has a Rigidbody, check to see if it is sleeping
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ((poiRigid != null) && poiRigid.IsSleeping())
+            {
+                POI = null;
+            }
+        }
+        if(POI != null)
+        {
+            destination = POI.transform.position;
+        }
+        // Limit the minimum values of destination.x & destination.y
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+        // Interpolate from the current Camera position toward destination
+        destination = Vector3.Lerp(transform.position, destination, easing);
         //Force destination.z to be camZ to keep the camera far enough away
         destination.z = camZ;
         // Set the camera to the destination
         transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 }
