69fc896d0ac83eb7e817733f892cb4b9d9dcbc4e
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-10-18 12:16:10 -0500

    added view script to MissionDemolition.cs

--- Assets/__Scripts/MissionDemolition.cs
+++ Assets/__Scripts/MissionDemolition.cs
@@ -60,6 +60,9 @@ public class MissionDemolition : MonoBehaviour
         UpdateGUI();
 
         mode = GameMode.playing;
+
+        // Zoom out to show both
+        FollowCam.SWITCH_VIEW(FollowCam.eView.both);
     }
 
     void UpdateGUI()
@@ -79,6 +82,9 @@ public class MissionDemolition : MonoBehaviour
         {
             // Change mode to stop checkin for level end
             mode = GameMode.levelEnd;
+
+            // Zoom out to show both
+            FollowCam.SWITCH_VIEW(FollowCam.eView.both);
 
             // Start the next level in 2 seconds
             Invoke("NextLevel", 2f);
