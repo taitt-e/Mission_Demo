aabc6022e2206a2ed3709a3d73d317ed2b3b2fb9
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-10-18 03:37:58 -0500

    Made ProjectileLine and Script for it

---
+++ Assets/__Scripts/ProjectileLine.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLine : MonoBehaviour
+{
+    private LineRenderer _line;
+    private bool _drawing = true;
+    private Projectile _projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _line = GetComponent<LineRenderer>();
+        _line.positionCount = 1;
+        _line.SetPosition(0, transform.position);
+
+        _projectile = GetComponentInParent<Projectile>();
+    }
+    void FixedUpdate()
+    {
+        if (_drawing)
+        {
+            _line.positionCount++;
+            _line.SetPosition(_line.positionCount - 1, transform.position);
+            // If the Projectile Rigidbody is sleeping, stop drawing
+            if(_projectile != null)
+            {
+                if (!_projectile.awake)
+                {
+                    _drawing = false;
+                    _projectile = null;
+                }
+            }
+        }
+    }
+}
