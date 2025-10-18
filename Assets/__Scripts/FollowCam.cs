5961afc5fac81d57cc715eefb90b1d748a35009c
taitt-e <123587963+taitt-e@users.noreply.github.com>
2025-10-18 12:03:02 -0500

    Made a camera view swapping button.

--- Assets/__Scripts/FollowCam.cs
+++ Assets/__Scripts/FollowCam.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+    static private FollowCam S; // Another private Singleton
     static public GameObject POI; // The Static point of interest
+    public enum eView{none, slingshot, castle, both};
     [Header("Inscribed")]
     public float easing = 0.05f;
     public Vector2 minXY = Vector2.zero; // Vector2.zero is [0,0]
+    public GameObject viewBothGO;
 
     [Header("Dynamic")]
     public float camZ; // The desired Z pos of the camera
+    public eView nextView = eView.slingshot;
 
     void Awake()
     {
+        S = this;
         camZ = this.transform.position.z;
     }
 
@@ -29,7 +34,7 @@ public class FollowCam : MonoBehaviour
                 POI = null;
             }
         }
-        if(POI != null)
+        if (POI != null)
         {
             destination = POI.transform.position;
         }
@@ -44,5 +49,38 @@ public class FollowCam : MonoBehaviour
         transform.position = destination;
         // Set the orthographicSize of the Camera to keep Ground in view
         Camera.main.orthographicSize = destination.y + 10;
+    }
+
+    public void SwitchView(eView newView)
+    {
+        if (newView == eView.none)
+        {
+            newView = nextView;
+        }
+        switch (newView)
+        {
+            case eView.slingshot:
+                POI = null;
+                nextView = eView.castle;
+                break;
+            case eView.castle:
+                POI = MissionDemolition.GET_CASTLE();
+                nextView = eView.both;
+                break;
+            case eView.both:
+                POI = viewBothGO;
+                nextView = eView.slingshot;
+                break;
+        }
+    }
+
+    public void SwitchView()
+    {
+        SwitchView(eView.none);
+    }
+    
+    static public void SWITCH_VIEW(eView newView)
+    {
+        S.SwitchView(newView);
     }
 }
